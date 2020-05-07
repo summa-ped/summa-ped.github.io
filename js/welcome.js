d24bea518caa9bbfdb9d82d2c02981567ac71bf6
Rafael Machado <rafafsmachado@gmail.com>
2020-05-07 04:10:20 -0300

    summa brand animation

--- js/welcome.js
+++ js/welcome.js
@@ -7,7 +7,7 @@ var
     forwards = true,
     skip_count = 0,
     skip_delay = 5,
-    speed = 100;
+    speed = 150;
 
 var wordflick = function () {
     setInterval(function () {
@@ -43,20 +43,79 @@ var wordflick = function () {
     }, speed);
 };
 
+var wordback = function () {
+    setInterval(function () {
+        if (forwards) {
+            if (offset >= words[i].length) {
+                ++skip_count;
+                if (skip_count == skip_delay) {
+                    forwards = false;
+                    skip_count = 0;
+                }
+            }
+        }
+        else {
+            if (offset == 0) {
+                forwards = true;
+                i++;
+                offset = 0;
+                if (i >= len) {
+                    i = 0;
+                }
+            }
+        }
+        part = words[i].substr(0, offset);
+        if (skip_count == 0) {
+            offset--;
+        }
+        if (part.length > 1) {
+            $('.word').text(part);
+        } else {
+            $('.word').text('');
+        }
+    }, speed);
+};
+
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-var welcome = function () {
+var showWelcome = function () {
     const logo = document.querySelector('.logo');
     const word = document.querySelector('.word');
     logo.style.transform = 'translate(-400px) rotate(-60deg)';
     word.style.transform = 'translate(-400px)';
 
     sleep(1000).then(() => { wordflick(); });
+    sleep(2000).then(() => { grow('.full-welcome'); });
+}
+
+var showBrand = function () {
+    const logo = document.querySelector('.logo');
+    const word = document.querySelector('.word');
+    const brand = document.querySelector('.brand');
+    const brandbox = document.querySelector('.brand-box');
+
+    wordback();
+    
+    sleep(1000).then(() => { logo.style.transform = 'none'; });
+    sleep(1500).then(() => { word.style.transform = 'none'; });
+
+    sleep(3000).then(() => { logo.style = 'display: none'; });
+    sleep(3000).then(() => { brand.style = 'display: block'; });
+    sleep(3000).then(() => { brandbox.style.transform = 'translate(0px, -25%)'; });
+    sleep(3000).then(() => { word.style = 'display: none'; });
+}
+
+var grow = function (classe) {
+    const el = document.querySelector(classe);
+
+    el.style.transform = 'scale(1.15)';
+    sleep(1500).then(() => { el.style.transform = 'scale(1)'; });
 }
 
 $(document).ready(function () {
-    document.querySelector('.logo').style.transform = 'scale(1.15)'
-    sleep(1000).then(() => { welcome(); });
+    //document.querySelector('.logo').style.transform = 'scale(1.15)'
+    sleep(1000).then(() => { showWelcome(); });
+    sleep(5000).then(() => { showBrand(); });
 });
