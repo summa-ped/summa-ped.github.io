d7a30a52f5a0a2c819fe77e724111e7457994f32
Rafael Machado <rafafsmachado@gmail.com>
2020-05-18 14:54:43 -0300

    typing

---
+++ js/typing.js
@@ -0,0 +1,49 @@
+var
+    words = ['Science', 'Data'],
+    part,
+    i = 0,
+    offset = 0,
+    len = words.length,
+    forwards = true,
+    skip_count = 0,
+    skip_delay = 5,
+    speed = 150,
+    under = '_';
+
+var wordflick = function () {
+    var inter = setInterval(function () {
+        if (forwards) {
+            if (offset >= words[i].length) {
+                ++skip_count;
+                if (skip_count == skip_delay) {
+                    forwards = false;
+                    skip_count = 0;
+                }
+            }
+        }
+        else {
+            if (offset == 0) {
+                forwards = true;
+                i++;
+                offset = 0;
+                if (i >= len) {
+                    i = 0;
+                }
+            }
+        }
+        part = words[i].substr(0, offset);
+        if (skip_count == 0) {
+            if (forwards) {
+                offset++;
+            }
+            else {
+                offset--;
+            }
+        }
+        $('.typing').text('We love ' + part + under);
+    }, speed);
+}
+
+$(document).ready(function () {
+    wordflick();
+});
