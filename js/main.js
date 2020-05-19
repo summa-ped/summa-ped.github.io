427313dc8a97297f0de8ae34bf4552209d3cd17e
Rafael Machado <rafafsmachado@gmail.com>
2020-05-18 23:27:05 -0300

    tech carousel

--- js/main.js
+++ js/main.js
@@ -9,3 +9,43 @@ function changeLang(lang){
 }
 
 $(".parallax").height($(window).height());
+
+$(document).ready(function(){
+    $('#tech').slick({
+        dots: false,
+        arrows: false,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 1,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 1
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+          // You can unslick at a given breakpoint now by adding:
+          // settings: "unslick"
+          // instead of a settings object
+        ]
+    });
+  });
