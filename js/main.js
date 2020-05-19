6d592cb8260a421a3c04c30dd82f7e6b62a6a79f
Rafael Machado <rafafsmachado@gmail.com>
2020-05-19 01:54:59 -0300

    background images, fonts and some details

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-function changeLang(lang){
-    if (lang == 0){
+function changeLang(lang) {
+    if (lang == 0) {
         $("#en").removeClass("active");
         $("#pt").addClass("active");
-    }else{
+    } else {
         $("#pt").removeClass("active");
         $("#en").addClass("active");
     }
@@ -10,7 +10,43 @@ function changeLang(lang){
 
 $(".parallax").height($(window).height());
 
-$(document).ready(function(){
+$(".menuHid").height($(window).height());
+function openContact() {
+    $("#openContact").fadeOut("slow", function () {
+        $("#sendMsg").fadeIn();
+        $("#txtArea").fadeIn();
+    });
+
+}
+function openInNewTab(url) {
+    var win = window.open(url, '_blank');
+    win.focus();
+}
+function sendMsg() {
+    $("#sendMsg").fadeOut("slow");
+    $("#txtArea").fadeOut("slow", function () {
+        $("#openContact").fadeIn();
+    });
+    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+        openInNewTab("https://api.whatsapp.com/send?text=" + $("#txtArea").val() + "&phone=5535999144165");
+    } else {
+        openInNewTab("https://web.whatsapp.com/send?text=" + $("#txtArea").val() + "&phone=5535999144165");
+    }
+}
+
+$("#btn").click(function () {
+    $(".menuHid").fadeToggle("slow", function () {
+        $("#li1").fadeToggle("slow", function () {
+            $("#li2").fadeToggle("slow", function () {
+                $("#li3").fadeToggle("slow", function () {
+                    $("#li4").fadeToggle("slow");
+                });
+            });
+        });
+    });
+});
+
+$(document).ready(function () {
     $('#tech').slick({
         dots: false,
         arrows: false,
@@ -20,67 +56,32 @@ $(document).ready(function(){
         slidesToShow: 5,
         slidesToScroll: 1,
         responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 1,
-              infinite: true,
-              dots: true
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
             }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 1
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-          // You can unslick at a given breakpoint now by adding:
-          // settings: "unslick"
-          // instead of a settings object
+            // You can unslick at a given breakpoint now by adding:
+            // settings: "unslick"
+            // instead of a settings object
         ]
-    });
-  });
-$(".menuHid").height($(window).height());
-function openContact(){
-    $("#openContact").fadeOut("slow", function () {
-        $("#sendMsg").fadeIn();
-        $("#txtArea").fadeIn();
-    });
-    
-}
-function openInNewTab(url) {
-    var win = window.open(url, '_blank');
-    win.focus();
-}
-function sendMsg(){
-    $("#sendMsg").fadeOut("slow");
-    $("#txtArea").fadeOut("slow", function () {
-        $("#openContact").fadeIn();
-    });
-    if( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
-        openInNewTab("https://api.whatsapp.com/send?text="+$("#txtArea").val()+"&phone=5535999144165");
-    }else{
-        openInNewTab("https://web.whatsapp.com/send?text="+$("#txtArea").val()+"&phone=5535999144165");
-    }
-}
-
-$("#btn").click(function (){
-    $(".menuHid").fadeToggle("slow", function () {
-        $("#li1").fadeToggle("slow", function () {
-            $("#li2").fadeToggle("slow", function () {
-                $("#li3").fadeToggle("slow", function () {
-                    $("#li4").fadeToggle("slow");
-                });        
-            });        
-        });        
     });
 });
